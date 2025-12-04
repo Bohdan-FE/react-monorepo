d52b6b646f33157f74902dc599755214662a0f99
Bohdan <bohdan.vivchar@qorpo.world>
2025-12-04 19:38:30 +0100

    Update api.ts

--- apps/dashboard/src/api/api.ts
+++ apps/dashboard/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'http://13.49.226.36:4252';
 //  'https://express-h9wn.onrender.com';
 
 const api = axios.create({
